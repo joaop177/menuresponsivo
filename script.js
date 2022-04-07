1909b7f0f4e083c0a1cfd44ef6b12a1bc6015fc7
João Paulo <42983746+joaop177@users.noreply.github.com>
2022-04-06 22:31:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// função para abrir e fechar menu lateral
+
+function openNav(){
+    document.getElementById("myNav").style.width = '25%';
+}
+
+function closeNav(){
+    document.getElementById("myNav").style.width = '0%';
+}
